a99e23c19a39feabb30dceacc80b9bc4fb4c5430
James Bird <jbrd@users.noreply.github.com>
2017-02-17 20:36:24 +0000

    Simplify validation code in repository module by factoring into Path

--- lib/begin/path.rb
+++ lib/begin/path.rb
@@ -2,8 +2,9 @@ module Begin
   # The canonical file path representation used throughout the application.
   # Paths are immediately expanded into absolute file paths on construction
   class Path
-    def initialize(path, parent_dir = nil)
+    def initialize(path, parent_dir, help)
       @path = File.expand_path path, parent_dir
+      @help = help
     end
 
     def to_s
@@ -12,6 +13,31 @@ module Begin
 
     def to_str
       @path
+    end
+
+    def ensure_exists
+      return if File.exist? @path
+      raise IOError, "#{@help} '#{@path}' does not exist"
+    end
+
+    def ensure_dir_exists
+      ensure_exists
+      return if File.directory? @path
+      raise IOError, "#{@help} '#{@path}' is not a directory"
+    end
+
+    def ensure_dir_is_empty
+      ensure_dir_exists
+      return if Dir.glob(File.join([path, '*'])).empty?
+      raise IOError, "#{@help} '#{@path}' is not empty"
+    end
+
+    def make_dir
+      unless File.exist? @path
+        Begin::Output.action "Making #{@help} '#{@path}'"
+        Dir.mkdir @path
+      end
+      ensure_dir_exists
     end
   end
 end
--- lib/begin/repository.rb
+++ lib/begin/repository.rb
@@ -8,41 +8,12 @@ module Begin
   # on the machine
   class Repository
     def initialize(name = '.begin', parent_dir = '~')
-      @parent_dir = Path.new parent_dir
-      check_parent_dir
-      @repo_dir = Path.new name, @parent_dir
-      check_repo_dir
-      @template_dir = Path.new 'templates', @repo_dir
-      check_template_dir
-    end
-
-    def check_parent_dir
-      # Parent dir must be a directory and must exist
-      unless File.exist? @parent_dir
-        raise IOError, "Repository parent '#{@parent_dir}' does not exist"
-      end
-      return if File.directory? @parent_dir
-      raise IOError, "Repository parent '#{@parent_dir}' is not a directory"
-    end
-
-    def check_repo_dir
-      # Repo folder can be created on demand if it doesn't
-      # already exist but must always be a directory.
-      unless File.exist? @repo_dir
-        Begin::Output.action "Making directory '#{@parent_dir}'"
-        Dir.mkdir @repo_dir
-      end
-      return if File.directory? @repo_dir
-      raise IOError, "Repository directory '#{@repo_dir}' is not a directory"
-    end
-
-    def check_template_dir
-      unless File.exist? @template_dir
-        Begin::Output.action "Making directory '#{@template_dir}'"
-        Dir.mkdir @template_dir
-      end
-      return if File.directory? @template_dir
-      raise IOError, "Template directory '#{@template_dir}' is not a directory"
+      @parent_dir = Path.new(parent_dir, '.', 'Repository Parent')
+      @parent_dir.ensure_dir_exists
+      @repo_dir = Path.new(name, @parent_dir, 'Repository directory')
+      @repo_dir.make_dir
+      @template_dir = Path.new('templates', @repo_dir, 'Templates directory')
+      @template_dir.make_dir
     end
 
     def list
@@ -53,7 +24,7 @@ module Begin
       name = template_name source_uri
       Output.action "Installing template '#{name}' from '#{source_uri}'"
       path = template_path name
-      check_template_path path
+      path.ensure_dir_exists
       Output.info "Installing to '#{path}'"
       try_install source_uri, path
       Output.success "Template '#{name}' successfully installed"
@@ -80,8 +51,8 @@ module Begin
     end
 
     def try_install_local_template(source_uri, path)
-      source_path = Path.new source_uri
-      check_local_template_source source_path
+      source_path = Path.new source_uri, '.', 'Source path'
+      source_path.ensure_dir_exists
       begin
         File.symlink(source_path, path)
         Output.success "Created symbolic link to '#{source_path}'"
@@ -90,16 +61,8 @@ module Begin
       end
     end
 
-    def check_local_template_source(source_path)
-      unless File.exist? source_path
-        raise IOError, "Template source '#{source_path}' was not reachable"
-      end
-      return if File.directory? source_path
-      raise IOError, "Template source '#{source_path}' is not a directory"
-    end
-
-    def template_name(source_uri)
-      uri = URI(source_uri)
+    def template_name(uri)
+      uri = URI(uri)
       path_bits = uri.path.split '/'
       name = path_bits.last
       name = name[6, name.length - 1] if name.start_with? 'begin-'
@@ -108,16 +71,7 @@ module Begin
     end
 
     def template_path(template_name)
-      File.join [@template_dir, template_name]
-    end
-
-    def check_template_path(path)
-      return unless File.exist? path
-      unless File.directory? path
-        raise IOError, "Template directory '#{path}' is not a directory"
-      end
-      return if Dir.glob(File.join([path, '*'])).empty?
-      raise IOError, "Template already installed at '#{path}'"
+      Path.new template_name, @template_dir, 'Template directory'
     end
   end
 end
